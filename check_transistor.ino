c0756aa31c9545a306ec0c902b54f1c0681c7238
SemanAlexsandrovich <sema.shmykov@mail.ru>
2024-10-29 19:46:27 +0300

    does not distinguish npn and pnp

--- check_transistor.ino
+++ check_transistor.ino
@@ -1,9 +1,9 @@
-//#include <U8g2lib.h>
-//#include <SPI.h>
-//#include <Wire.h>
+#include <U8g2lib.h>
+#include <SPI.h>
+#include <Wire.h>
 #include "GyverButton.h"
 
-//U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);
 
 #define BUTTON_PIN 2
 
@@ -31,7 +31,7 @@ void reset_transistor(){
   digitalWrite(TEST_PIN3, LOW);
 }
 
-check_status_t check_field_effect_transistor(int gate, int source, int drain){
+check_status_t check_field_effect_transistor(int gate, int drain, int source){
   char test_pin1,test_pin2,test_pin3;
   int gate_close_value = 0;
   int gate_open_value = 0;
@@ -67,50 +67,42 @@ check_status_t check_field_effect_transistor(int gate, int source, int drain){
       }
     }
   }
-  //закрываем считываем 1 открываем считываем 2 сравниваем 1 и 2
   //check N channel
   reset_transistor();
   digitalWrite(gate, HIGH);
   digitalWrite(source, LOW);
   delay(10);
   digitalWrite(drain, HIGH);
-  //digitalWrite(drain, LOW);
   gate_close_value = analogRead(analog_pin[source]);
   digitalWrite(gate, LOW);
   digitalWrite(source, HIGH);
   delay(10);
   digitalWrite(source, LOW);
   gate_open_value = analogRead(analog_pin[source]);
-  if (gate_open_value < 1000) {//need to analog way to separate bipolar from fied transistor
-    return CHECK_FAIL;
-  }
-  if (gate_open_value > gate_close_value && gate_open_value - gate_close_value > 100 && gate_close_value != 0) { 
-    char debug_str[40] = "";
-    sprintf(debug_str, "open=%d, close=%d",gate_open_value, gate_close_value);
-    Serial.println(debug_str);
+  char debug_str[40] = "";
+  sprintf(debug_str, "open=%d, close=%d",gate_open_value, gate_close_value);
+  Serial.println(debug_str);
+  if (gate_open_value > gate_close_value && gate_close_value > 20 && gate_close_value < 1000) { //not a safe condition: gate_close_value > 20 && gate_open_value < 1000
     sprintf(oled_str, "%c%c%c - N",test_pin1, test_pin2, test_pin3);
     return CHECK_OK;
   } else {
     //check P channel
     reset_transistor();
     digitalWrite(gate, LOW);
-    digitalWrite(source, HIGH);
+    digitalWrite(drain, HIGH);
     delay(10);
-    digitalWrite(drain, LOW);
-    //digitalWrite(drain, HIGH);
-    gate_close_value = analogRead(analog_pin[drain]);
-    digitalWrite(gate, HIGH);
     digitalWrite(source, LOW);
+    //digitalWrite(drain, HIGH);
+    gate_close_value = analogRead(analog_pin[source]);
+    digitalWrite(gate, HIGH);
+    digitalWrite(drain, LOW);
     delay(10);
-    digitalWrite(source, HIGH);
-    gate_open_value = analogRead(analog_pin[drain]);
-    if (gate_open_value < 1000) {//need to analog way to separate bipolar from fied transistor
-      return CHECK_FAIL;
-    }
-    if (gate_open_value > gate_close_value && gate_open_value - gate_close_value > 100 && gate_close_value != 0) {
-      char debug_str[40] = "";
-      sprintf(debug_str, "open=%d, close=%d",gate_open_value, gate_close_value);
-      Serial.println(debug_str);
+    digitalWrite(drain, HIGH);
+    gate_open_value = analogRead(analog_pin[source]);
+    char debug_str[40] = "";
+    sprintf(debug_str, "open=%d, close=%d",gate_open_value, gate_close_value);
+    Serial.println(debug_str);
+    if (gate_open_value > gate_close_value && gate_close_value > 20 && gate_close_value < 1000) {
       sprintf(oled_str, "%c%c%c - P",test_pin1, test_pin2, test_pin3);
       return CHECK_OK;
     }
@@ -158,10 +150,17 @@ check_status_t check_bipolar_effect_transistor(int emitter, int collector, int b
   //check pnp
   digitalWrite(base, LOW);
   digitalWrite(emitter, HIGH);
+  digitalWrite(collector, LOW);
+  delay(10);
+  emitter_value = analogRead(analog_pin[base]);
+  digitalWrite(emitter, LOW);
   digitalWrite(collector, HIGH);
-  emitter_value = analogRead(analog_pin[collector]);
-  collector_value = analogRead(analog_pin[emitter]);
-  if (emitter_value > collector_value && emitter_value - collector_value > 50 && collector_value != 0) {
+  delay(10);
+  collector_value = analogRead(analog_pin[base]);
+  char debug_str[40] = "";
+  sprintf(debug_str, "emitter=%d, collector=%d", emitter_value, collector_value);
+  Serial.println(debug_str);
+  if (emitter_value > collector_value && emitter_value - collector_value > 50 && collector_value > 20 && emitter_value < 1000) {//not a safe condition: collector_value > 20 && emitter_value < 1000
     sprintf(oled_str, "%c%c%c-pnp",test_pin1, test_pin2, test_pin3);
     return CHECK_OK;
   } else {
@@ -171,7 +170,10 @@ check_status_t check_bipolar_effect_transistor(int emitter, int collector, int b
     digitalWrite(collector, LOW);
     emitter_value = analogRead(analog_pin[collector]);
     collector_value = analogRead(analog_pin[emitter]);
-    if (emitter_value > collector_value && emitter_value - collector_value > 50 && collector_value != 0) {
+    char debug_str[40] = "";
+    sprintf(debug_str, "emitter=%d, collector=%d", emitter_value, collector_value);
+    Serial.println(debug_str);
+    if (emitter_value > collector_value && emitter_value - collector_value > 50 && collector_value > 20  && emitter_value < 1000) {
       sprintf(oled_str, "%c%c%c-npn",test_pin1, test_pin2, test_pin3);
       return CHECK_OK;
     }
@@ -179,23 +181,47 @@ check_status_t check_bipolar_effect_transistor(int emitter, int collector, int b
   return CHECK_FAIL;
 }
 
+/*
+check_status_t check_transistor_effect(int pin1, int pin2, int pin3){
+  if (check_bipolar_effect_transistor(pin1, pin2, pin3) == CHECK_FAIL){
+    if (check_field_effect_transistor(pin1, pin2, pin3) == CHECK_FAIL){
+      return CHECK_FAIL;
+    }
+  }
+  return CHECK_OK;
+---------
+  if (check_transistor_effect(TEST_PIN1, TEST_PIN2, TEST_PIN3) == CHECK_FAIL){
+    if (check_transistor_effect(TEST_PIN1, TEST_PIN3, TEST_PIN2) == CHECK_FAIL){
+      if (check_transistor_effect(TEST_PIN2, TEST_PIN1, TEST_PIN3) == CHECK_FAIL){
+        if (check_transistor_effect(TEST_PIN2, TEST_PIN3, TEST_PIN1) == CHECK_FAIL){
+          if (check_transistor_effect(TEST_PIN3, TEST_PIN2, TEST_PIN1) == CHECK_FAIL){
+            if (check_transistor_effect(TEST_PIN3, TEST_PIN1, TEST_PIN2) == CHECK_FAIL){
+              sprintf(oled_str, "fail");
+            }
+          }
+        }
+      }
+    }
+  }
+}*/
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(TEST_PIN1, OUTPUT);
   pinMode(TEST_PIN2, OUTPUT);
   pinMode(TEST_PIN3, OUTPUT);
-  btn.setDebounce(50);        // настройка антидребезга (по умолчанию 80 мс)
-  btn.setTimeout(300);        // настройка таймаута на удержание (по умолчанию 500 мс)
-  btn.setClickTimeout(600);   // настройка таймаута между кликами (по умолчанию 300 мс)
+  btn.setDebounce(50);
+  btn.setTimeout(300);
+  btn.setClickTimeout(600);
   btn.setType(HIGH_PULL);
   btn.setDirection(NORM_OPEN);
 
   digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(9600);
-  //u8g2.begin();
-  //u8g2.setFont(u8g2_font_logisoso28_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
-  //u8g2.drawStr(8,29,"MYBOTIC");	// write something to the internal memory
-  //u8g2.sendBuffer();					// transfer internal memory to the display
+  u8g2.begin();
+  u8g2.setFont(u8g2_font_logisoso28_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
+  u8g2.drawStr(8,29,"MYBOTIC");
+  u8g2.sendBuffer();
 }
 
 void loop() {
@@ -203,18 +229,22 @@ void loop() {
   if (btn.isRelease()) {   
     digitalWrite(LED_BUILTIN, HIGH);
     Serial.println("task execution...");
-    if (check_field_effect_transistor(TEST_PIN1, TEST_PIN2, TEST_PIN3) == CHECK_FAIL){
-      if (check_field_effect_transistor(TEST_PIN1, TEST_PIN3, TEST_PIN2) == CHECK_FAIL){
-        if (check_field_effect_transistor(TEST_PIN2, TEST_PIN1, TEST_PIN3) == CHECK_FAIL){
-          if (check_field_effect_transistor(TEST_PIN2, TEST_PIN3, TEST_PIN1) == CHECK_FAIL){
-            if (check_field_effect_transistor(TEST_PIN3, TEST_PIN2, TEST_PIN1) == CHECK_FAIL){
-              if (check_field_effect_transistor(TEST_PIN3, TEST_PIN1, TEST_PIN2) == CHECK_FAIL){
-                if (check_bipolar_effect_transistor(TEST_PIN1, TEST_PIN2, TEST_PIN3) == CHECK_FAIL){
-                  if (check_bipolar_effect_transistor(TEST_PIN1, TEST_PIN3, TEST_PIN2) == CHECK_FAIL){
-                    if (check_bipolar_effect_transistor(TEST_PIN2, TEST_PIN1, TEST_PIN3) == CHECK_FAIL){
-                      if (check_bipolar_effect_transistor(TEST_PIN2, TEST_PIN3, TEST_PIN1) == CHECK_FAIL){
-                        if (check_bipolar_effect_transistor(TEST_PIN3, TEST_PIN2, TEST_PIN1) == CHECK_FAIL){
-                          if (check_bipolar_effect_transistor(TEST_PIN3, TEST_PIN1, TEST_PIN2) == CHECK_FAIL){
+    u8g2.clearBuffer(); 
+    u8g2.drawStr(8,29,"testing");
+    u8g2.sendBuffer();
+    delay(10);
+    if (check_bipolar_effect_transistor(TEST_PIN1, TEST_PIN2, TEST_PIN3) == CHECK_FAIL){
+      if (check_bipolar_effect_transistor(TEST_PIN1, TEST_PIN3, TEST_PIN2) == CHECK_FAIL){
+        if (check_bipolar_effect_transistor(TEST_PIN2, TEST_PIN1, TEST_PIN3) == CHECK_FAIL){
+          if (check_bipolar_effect_transistor(TEST_PIN2, TEST_PIN3, TEST_PIN1) == CHECK_FAIL){
+            if (check_bipolar_effect_transistor(TEST_PIN3, TEST_PIN2, TEST_PIN1) == CHECK_FAIL){
+              if (check_bipolar_effect_transistor(TEST_PIN3, TEST_PIN1, TEST_PIN2) == CHECK_FAIL){
+                if (check_field_effect_transistor(TEST_PIN1, TEST_PIN2, TEST_PIN3) == CHECK_FAIL){
+                  if (check_field_effect_transistor(TEST_PIN1, TEST_PIN3, TEST_PIN2) == CHECK_FAIL){
+                    if (check_field_effect_transistor(TEST_PIN2, TEST_PIN1, TEST_PIN3) == CHECK_FAIL){
+                      if (check_field_effect_transistor(TEST_PIN2, TEST_PIN3, TEST_PIN1) == CHECK_FAIL){
+                        if (check_field_effect_transistor(TEST_PIN3, TEST_PIN2, TEST_PIN1) == CHECK_FAIL){
+                          if (check_field_effect_transistor(TEST_PIN3, TEST_PIN1, TEST_PIN2) == CHECK_FAIL){
                             sprintf(oled_str, "fail");
                           }
                         }
@@ -230,14 +260,13 @@ void loop() {
     }
     reset_transistor();
     Serial.println(oled_str);
-    //u8g2.drawStr(x,32,oled_str);
-    //u8g2.sendBuffer();
-    //delay(10);
+    u8g2.clearBuffer(); 
+    u8g2.drawStr(0,29,oled_str);
+    u8g2.sendBuffer();
+    delay(10);
     for (int i = 0; i < STR_SIZE; i++){
       oled_str[i] = ' ';
     }
     digitalWrite(LED_BUILTIN, LOW);
-
-
   }
 }
